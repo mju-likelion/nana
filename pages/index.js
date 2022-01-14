4d1161f601dc516aaa95f93908175f3637b62152
Bhanjo <hanjo1515@naver.com>
2022-01-15 02:47:33 +0900

    추가: 예시 컴포넌트 추가 및 styled-components 적용 안되는 문제 수정

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,8 @@ import Test from "../components/test";
 export default function Home() {
   return (
     <>
-      <h1>멋쟁이 사자처럼 홍보사이트</h1>
+      <h1>멋쟁이 사자처럼 홍보사이트 메인</h1>
+      <p>여기에 다양한 컴포넌트를 불러와 보세요</p>
       <Test />
     </>
   );
