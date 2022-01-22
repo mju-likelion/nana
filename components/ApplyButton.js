e3341828c2c070c4633ac86fab7fb3754d2df967
Bhanjo <hanjo1515@naver.com>
2022-01-22 18:24:47 +0900

    feature: 지원 버튼 추가

---
+++ components/ApplyButton.js
@@ -0,0 +1,34 @@
+import styled from 'styled-components';
+
+const ApplyBtn = styled.button`
+  /* props에 수치가 들어왔으면 해당 수치대로 실행하고, 없다면 기본 수치를 적용합니다 */
+  width: ${(props) => props.width || '100px'};
+  height: ${(props) => props.height || '50px'};
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 0 auto;
+  background: #fff;
+  color: #000;
+  font-size: 16px;
+  font-weight: bolder;
+
+  img {
+    width: 40px;
+    height: 15px;
+  }
+`;
+
+// props로 text, width, height을 받아옵니다
+const ApplyButton = ({ text, width, height }) => {
+  return (
+    // 받아온 props를 스타일링을 위한 수치로 활용합니다
+    <ApplyBtn width={width} height={height}>
+      {text}
+      <img src='../svg/ic-more.svg' alt='화살표' />
+    </ApplyBtn>
+  );
+};
+
+export default ApplyButton;
