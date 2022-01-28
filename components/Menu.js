0764ad11f416a8812214cd18c18873940d36c01e
Soo Jeong <totnwjd530@gmail.com>
2022-01-28 13:37:11 +0900

    style: 전체적인 메뉴 틀

---
+++ components/Menu.js
@@ -0,0 +1,48 @@
+// MJU, CURRICULUM, OUTPUT, JOIN 메뉴 컴포넌트
+import styled from 'styled-components';
+
+const MenuWrap = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 485px;
+  height: 25px;
+
+  @media screen and (max-width: 1024px) {
+    flex-direction: column;
+    /* 세로변환 시 메뉴들 센터로 -> 너비는 넓게 100% */
+    align-items: center;
+    width: 100%;
+  }
+`;
+
+const MenuBtn = styled.button`
+  color: #FFFFFF;
+  font-weight: bold;
+  font-size: 12px;
+  padding: 0 28px;
+  transition: 0.2s;
+  cursor: pointer;
+
+  &:hover { 
+  padding-bottom: 8px;
+  }
+
+  /* 모바일 디자인 */
+  @media screen and (max-width: 1024px) {
+    margin: 10px 0;
+    padding: 30px 0;
+  }
+`;
+
+const Menu = () => {
+  return (
+    <MenuWrap>
+      <MenuBtn>LIKELION | MJU</MenuBtn>
+      <MenuBtn>CURRICULUM</MenuBtn>
+      <MenuBtn>OUTPUT</MenuBtn>
+      <MenuBtn>JOIN</MenuBtn>
+    </MenuWrap>
+  );
+};
+
+export default Menu;
