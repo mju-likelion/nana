86d34059cc68e672bb1bd3752f7c1f5bef25d212
Soo Jeong <totnwjd530@gmail.com>
2022-02-03 12:58:00 +0900

    style: 해커톤 정보배열 선언 위치 수정

--- components/OutPut.js
+++ components/OutPut.js
@@ -28,12 +28,40 @@ const ContentWrap = styled.div`
   }
 `;
 
+// 해커톤 서비스 정보배열
+const Hackathon = [
+  {
+    title: "준비됐멍 팀의 반려견 입양 준비도 테스트 & 커뮤니티",
+    serviceImg: "../Img/walwal.png",
+  },
+  {
+    title: "라온에어 팀의 연극, 극단을 찾아볼 수 있는 서비스", 
+    serviceImg: "../Img/raonAir.png",
+  },
+  {
+    title: "Maplog 팀의 그 날의 기록을 지도로 한 눈에 보는 서비스",
+    serviceImg: "../Img/mapLog.png",
+  },
+  {
+    title: "Man’s Skin 팀의 그루밍 정보를 빠르게 제공하는 서비스",
+    serviceImg: "../Img/mansSkin.png",
+  },
+  {
+    title: "Save Energy 팀의 대기 전력 계산을 통한 환경보호 서비스",
+    serviceImg: "../Img/saveEnergy.png",
+  },
+  {
+    title: "One can do it 팀의 1인 가구 맞춤형 마켓 & 커뮤니티",
+    serviceImg: "../Img/oneCan.png",
+  },
+]
+
 const OutPut = () => {
   return (
     <OutPutWrap>
       <ContentWrap>
         <h2>우리는 이런 것을 만들었어요</h2>
-        <OutPutSlide />
+        <OutPutSlide Hackathon={Hackathon} />
       </ContentWrap>
     </OutPutWrap>
   );
