b269226175468521525e948886a70efe17f9d401
qkrthdus605 <qkrthdus605@gmail.com>
2022-02-03 21:03:38 +0900

    style: 모바일 버전 UI 제작

--- components/Join.js
+++ components/Join.js
@@ -10,6 +10,11 @@ const Wrapper = styled.div`
 
 // 동료상 컴포넌트 ColleagueBox
 const ColleagueBox = styled.div`
+  @media screen and (max-width: 1024px) {
+    width: 289px;
+    height: 183px;
+    margin-top: 117px;
+  }
   width: 974px;
   height: 469px;
   background: url(${'../img/colleague.svg'});
@@ -18,6 +23,11 @@ const ColleagueBox = styled.div`
 
 // 동료상 타이틀 컴포넌트 ColleagueTitle
 const ColleagueTitle = styled.p`
+  @media screen and (max-width: 1024px) {
+    font-size: 20px;
+    margin-left: 11px;
+    margin-top: 10px;
+  }
   font-size: 40px;
   font-weight: 800;
   color: white;
@@ -28,6 +38,12 @@ const ColleagueTitle = styled.p`
 
 // 동료상 내용 컴포넌트 ColleagueContent
 const ColleagueContent = styled.div`
+  @media screen and (max-width: 1024px) {
+    font-size: 10px;
+    margin-left: 30px;
+    margin-top: 20px;
+    line-height: 20px;
+  }
   font-size: 20px;
   font-weight: 800;
   color: white;
@@ -40,6 +56,11 @@ const ColleagueContent = styled.div`
 
 // 모집정보 컴포넌트 RecruitBox
 const RecruitBox = styled.div`
+  @media screen and (max-width: 1024px) {
+    width: 289px;
+    height: 183px;
+    margin-top: 33px;
+  }
   width: 974px;
   height: 469px;
   background: url(${'../img/recruit.svg'});
@@ -48,6 +69,11 @@ const RecruitBox = styled.div`
 
 // 모집정보 타이틀 컴포넌트 RecruitTitle
 const RecruitTitle = styled.p`
+  @media screen and (max-width: 1024px) {
+    font-size: 20px;
+    margin-left: 8px;
+    margin-top: 12px;
+  }
   font-size: 40px;
   font-weight: 800;
   color: white;
@@ -58,6 +84,12 @@ const RecruitTitle = styled.p`
 
 // 모집정보 내용 컴포넌트 RecruitContent
 const RecruitContent = styled.div`
+  @media screen and (max-width: 1024px) {
+    font-size: 10px;
+    margin-left: 30px;
+    margin-top: 20px;
+    line-height: 20px;
+  }
   font-size: 20px;
   font-weight: 800;
   color: white;
@@ -84,7 +116,9 @@ const Join = () => {
       <RecruitBox>
         <RecruitTitle>모집정보</RecruitTitle>
         <RecruitContent>
-          <p>● 모집기간 <br/> - 3월 2일(수) ~ 3월 11일(금) PM 11:59까지</p>
+          <p>
+            ● 모집기간 <br /> - 3월 2일(수) ~ 3월 11일(금) PM 11:59까지
+          </p>
           <p>● 모집인원(약25명-30명)</p>
           <p>● 기획, 디자인, 웹 파트로 나눠 지원받아요!</p>
         </RecruitContent>
