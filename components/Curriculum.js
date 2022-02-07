d1025568a15efe513d35d641a6a4f45d7225c71f
yuraup <o721@naver.com>
2022-02-07 21:17:29 +0900

    style: 커리큘럼(기술 파트) 모바일 개선

--- components/Curriculum.js
+++ components/Curriculum.js
@@ -19,33 +19,49 @@ const CurriContainer = styled.div`
   flex-direction: column;
   font-size: 30px;
 
-  img {
-    width: 854px;
-    height: 319px;
-  }
-
-  p {
-    width: 420px;
-    height: 22px;
-    margin-top: 8px;
-    margin-left: 520px;
-    font-style: normal;
-    font-weight: 900;
-    font-size: 16px;
-    line-height: 16px;
-    text-align: center;
-    color: #737070;
+  @media screen and (max-width: 1024px) {
+    width: 80%;
   }
 `;
+const CurriImgBox = styled.div`
+@media screen and (max-width: 1024px) {
+      justify-content: center;
+      img {
+        /* width: 10px; */
+        /* height: auto; */
+      }
+    }
+    img {
+      /* margin-left: 36px;
+      margin-bottom: 50px;
+      margin-right: 37px;
+      width: 840px;
+      height: 316px; */
+    }
+    background: blue;
+  }`;
 
+const CurriTextBox = styled.p`
+  @media screen and (max-width: 1024px) {
+    text-align: center;
+  }
+  width: 100%;
+  margin-top: 8px;
+  font-style: normal;
+  font-weight: 900;
+  font-size: 16px;
+  line-height: 16px;
+  text-align: right;
+  color: #737070;
+`;
 const PLText = styled.div`
   width: 413px;
   height: 21px;
-  margin-top: 49px;
   font-style: normal;
   font-weight: bold;
   font-size: 16px;
   line-height: 20px;
+  margin: 53px auto;
   text-align: center;
   color: #737070;
 `;
@@ -56,22 +72,51 @@ const PLImgContainer = styled.div`
   }
   display: flex;
 `;
-const PLImgBox = styled.div`
-  margin-top: 41px;
+const PLImgFirstBox = styled.div`
   display: flex;
+  justify-content: center;
+
   @media screen and (max-width: 1024px) {
-    justify-content: center;
-    img {
-      width: 30%;
-      height: auto;
+    margin-bottom: 15px;
+    img:nth-child(1) {
+      margin-right: 33px;
+      width: 150px;
+      height: 91px;
+    }
+    img:nth-child(2) {
+      margin: 0 33px;
+      width: 87px;
+      height: 87px;
     }
   }
   img {
-    margin-left: 36px;
-    margin-bottom: 50px;
-    margin-right: 37px;
-    width: 30%;
-    height: auto;
+    /* margin: 0 33px; */
+  }
+  img:nth-child(1) {
+    margin-right: 33px;
+  }
+  img:nth-child(2) {
+    margin: 0 33px;
+    width: ;
+  }
+`;
+const PLImgSecondBox = styled.div`
+  display: flex;
+  img:nth-child(1) {
+    margin: 0 33px;
+  }
+  img:nth-child(2) {
+    margin: 0 33px;
+  }
+  img:nth-child(3) {
+    margin-left: 33px;
+  }
+
+  @media screen and (max-width: 1024px) {
+    /* justify-content: space-between; */
+    img:nth-child(1) {
+      margin: 0 33px 0 0;
+    }
   }
 `;
 
@@ -79,22 +124,24 @@ const Curriculum = () => {
   return (
     <CurriWapper>
       <CurriContainer>
-        <img src='../img/fakeCurriculum.png' alt='커리큘럼사진예시' />
-        <p>*커리큘럼은 상황에 따라 변경될 수 있습니다.</p>
+        <CurriImgBox>
+          <img src='../img/fakeCurriculum.png' alt='커리큘럼사진예시' />
+        </CurriImgBox>
+        <CurriTextBox>*커리큘럼은 상황에 따라 변경될 수 있습니다.</CurriTextBox>
       </CurriContainer>
       <PLText>
         <p>Programming Language</p>
       </PLText>
       <PLImgContainer>
-        <PLImgBox>
+        <PLImgFirstBox>
           <img src='../img/htmlcss.png' alt='HtmlAndCss' />
           <img src='../img/react.png' alt='react' />
-        </PLImgBox>
-        <PLImgBox>
+        </PLImgFirstBox>
+        <PLImgSecondBox>
           <img src='../img/node.png' alt='node.js' />
           <img src='../img/javascript.png' alt='javascript' />
           <img src='../img/django.png' alt='django' />
-        </PLImgBox>
+        </PLImgSecondBox>
       </PLImgContainer>
     </CurriWapper>
   );
