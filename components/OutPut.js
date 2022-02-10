7feec3c4b42f8bf5945d7529480aa72141f51762
Soo Jeong <totnwjd530@gmail.com>
2022-02-10 22:03:03 +0900

    fix: editOutPut에서 merge한 페이지 오류 임시 수정

--- components/OutPut.js
+++ components/OutPut.js
@@ -47,32 +47,32 @@ const Hackathon = [
   {
     id: 0,
     title: '준비됐멍 팀의 반려견 입양 준비도 테스트 & 커뮤니티',
-    serviceImg: '../Img/walwal.png',
+    serviceImg: '../img/walwal.png',
   },
   {
     id: 1,
     title: '라온에어 팀의 연극, 극단을 찾아볼 수 있는 서비스',
-    serviceImg: '../Img/raonAir.png',
+    serviceImg: '../img/raonAir.png',
   },
   {
     id: 2,
     title: 'MapLog 팀의 그 날의 기록을 지도로 한 눈에 보는 서비스',
-    serviceImg: '../Img/mapLog.png',
+    serviceImg: '../img/mapLog.png',
   },
   {
     id: 3,
     title: 'Man’s Skin 팀의 그루밍 정보를 빠르게 제공하는 서비스',
-    serviceImg: '../Img/mansSkin.png',
+    serviceImg: '../img/mansSkin.png',
   },
   {
     id: 4,
     title: 'Save Energy 팀의 대기 전력 계산을 통한 환경보호 서비스',
-    serviceImg: '../Img/saveEnergy.png',
+    serviceImg: '../img/saveEnergy.png',
   },
   {
     id: 5,
     title: 'One can do it 팀의 1인 가구 맞춤형 마켓 & 커뮤니티',
-    serviceImg: '../Img/oneCan.png',
+    serviceImg: '../img/oneCan.png',
   },
 ];
 
