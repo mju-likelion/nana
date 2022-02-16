9cf4acdb0c6fea6ece0afd6911c7330b9b11f7e0
Bhanjo <hanjo1515@naver.com>
2022-02-17 00:13:00 +0900

    setting: smooth scroll behavior 패키지 설치

--- components/Menu.js
+++ components/Menu.js
@@ -1,5 +1,6 @@
 // LIKELION|MJU, CURRICULUM, OUTPUT, JOIN 메뉴 컴포넌트
 import { forwardRef } from 'react';
+import smoothscroll from 'smoothscroll-polyfill';
 import styled from 'styled-components';
 
 const MenuWrap = styled.div`
@@ -80,7 +81,9 @@ const Menu = forwardRef((props, ref) => {
   ];
 
   // href를 받아 string으로 변환한 후 해당 객체 까지 이동
-  const smoothScroll = (target) => {
+  const smoothScrollEvent = (target) => {
+    smoothscroll.polyfill();
+
     // herf 객체를 object로 변환 후 쌍따옴표를 기준으로 나눔 그 후 다시 #으로 나눈다
     const pointArr = JSON.stringify(target.href).split('"');
     const splitAddr = pointArr[1].split('#');
@@ -93,7 +96,7 @@ const Menu = forwardRef((props, ref) => {
 
   // 부드러운 스크롤을 위한 헨들러
   const handleScroll = (e) => {
-    smoothScroll(e.target);
+    smoothScrollEvent(e.target);
     e.preventDefault();
   };
 
