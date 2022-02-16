6d78d771c8044df0136c959abd9483fa22be2156
qkrthdus605 <qkrthdus605@gmail.com>
2022-02-17 01:58:15 +0900

    feature: 푸터 구현 완료 및 인스타그램 svg이미지 추가

--- components/Footer.js
+++ components/Footer.js
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 
 import Github from '../public/img/GitFooter.svg';
+import Insta from '../public/img/InstaFooter.svg';
 
 const FooterSelf = styled.div`
   width: 100%;
@@ -8,26 +9,46 @@ const FooterSelf = styled.div`
   display: flex;
   margin-top: auto;
   flex-direction: row;
+  justify-content: center;
   z-index: 100;
 `;
 
 const MjuLogo = styled.div`
   font-size: 15px;
-  margin: auto;
   color: white;
+  margin-top: 15px;
+`;
+
+const GitContainer = styled.div`
+  margin: 5px 0 0 30px;
+`;
+
+const InstaContainer = styled.div`
+  margin: 5px 0 0 10px;
 `;
 
 function Footer() {
   return (
     <FooterSelf>
       <MjuLogo>LIKE LION | MJU Nature © 2022</MjuLogo>
-      <a
-        href='https://github.com/mju-likelion/nana'
-        target='_blank'
-        rel='noreferrer'
-      >
-        <Github width='35' height='35' alt='깃허브로고' fill='none' />
-      </a>
+      <GitContainer>
+        <a
+          href='https://github.com/mju-likelion/nana'
+          target='_blank'
+          rel='noreferrer'
+        >
+          <Github width='35' height='35' alt='깃허브로고' fill='none' />
+        </a>
+      </GitContainer>
+      <InstaContainer>
+        <a
+          href='https://www.instagram.com/mju_likelion/'
+          target='_blank'
+          rel='noreferrer'
+        >
+          <Insta width='35' height='35' alt='인스타그램' fill='none' />
+        </a>
+      </InstaContainer>
     </FooterSelf>
   );
 }
